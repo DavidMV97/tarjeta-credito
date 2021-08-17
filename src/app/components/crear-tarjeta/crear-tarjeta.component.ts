f364ef575b848c48e0e31ce7c8bff10123f740dd
David <machov12@gmail.com>
2021-08-17 12:28:35 -0500

    formulario crear tarjeta

---
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-crear-tarjeta',
+  templateUrl: './crear-tarjeta.component.html',
+  styleUrls: ['./crear-tarjeta.component.scss']
+})
+export class CrearTarjetaComponent implements OnInit {
+  form: FormGroup; 
+
+  constructor( private fb: FormBuilder ) {
+    this.form = this.fb.group({
+      titular:[ '' , Validators.required ],
+      numeroTarjeta:[ '' , Validators.required ],
+      fechaExpiracion:[ '' , Validators.required ],
+      cvv:[ '' , Validators.required ]
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  crearTarjeta(){
+    console.log(this.form) ; 
+  }
+
+}
